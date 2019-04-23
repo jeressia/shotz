5d0b16e571f3b23fcc4bb7a38172002c26fd7fdd
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-22 19:30:57 -0500

    colored headers

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,11 +5,32 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div id="${location.id}" class="card location col-2">`;
-    domString += `<div class="card-header">${location.name}</div>`;
+    domString += `<div class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</div>`;
     domString += `<img class="card-img-top"> <img src="${location.imageUrl}">`;
     domString += '<ul class="list-group list-group-flush">';
     domString += `<li class="list-group-item address"><strong>Address</strong><br> ${location.address}</li>`;
